ee76e89598b613003a87da0b877abee9b71f67ee
Arcelow <117592575+Arcelow@users.noreply.github.com>
2022-11-29 17:31:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ print("""
 -------------------------------------------------------------------------------------------
 Pythagorean Theorem calculator! Enter '?' if it's unknown!
 Don't use non-numbers or '?'
+Use 2 numbers (any combination)!
+No negatives!
 -------------------------------------------------------------------------------------------
 """)
 A = input("A length: ")
 B = input("B length: ")
 C = input("C length: ")
 
+
 if C == "?":
-    combine_like_terms = float(A) ** 2 + float (B) ** 2
-    solution = math.sqrt(combine_like_terms)
-    print(solution)
-    if float(A) ** 2 + float(B) == solution:
-        print("This is also a real right triangle!")
-    else:
-        print("This is not a right triangle!")
+    if A or B == "?":
+        print("Not enough information!")
+        exit()
+    elif A or B != "?":
+        combine_like_terms = float(A) ** 2 + float(B) ** 2
+        solution = math.sqrt(combine_like_terms)
+        print(solution)
+        if float(A) ** 2 + float(B) == solution:
+            print("This is also a real right triangle!")
+        else:
+            print("This is not a right triangle!")
 elif B == "?":
     combine_like_terms = float(C) ** 2 - float(A) ** 2
     solution = math.sqrt(combine_like_terms)
     print(solution)
-    if float(A) ** 2 + float(B) == solution:
+    if float(A) ** 2 + solution ** 2 == float(C) ** 2:
         print("This is also a real right triangle!")
     else:
         print("This is not a right triangle!")
@@ -29,7 +36,7 @@ elif A == "?":
     combine_like_terms = float(C) ** 2 - float(B) ** 2
     solution = math.sqrt(combine_like_terms)
     print(solution)
-    if float(A) ** 2 + float(B) == solution:
+    if solution ** 2 + float(B) == solution:
         print("This is also a right1 triangle!")
     else:
         print("This is not a triangle!")
@@ -40,8 +47,6 @@ elif A and B and C != "?":
         print("This is also a real right triangle!")
     else:
         print("This is not a right triangle!")
-    print("Invalid character or too much information!")
+else:
+    print("Invalid characters or equation!")
 exit()
-
-
-
